19989510ba8151a1b3a92cad71c61046fce9863d
laptop_Maksim_Alekseev <maxim040597@gmail.com>
2023-07-04 23:32:21 +0300

    переход на PC

--- page.php
+++ page.php
@@ -6,7 +6,8 @@
 	<section class="container shop">
 		<div class="row">
 		
-				<div class="col-4 card">
+				<div class="col-4">
+					<div class="card">
 						<!-- Верхняя часть -->
 						<div class="card__top">
 							<!-- Изображение-ссылка товара -->
@@ -23,13 +24,87 @@
 							<!-- Ссылка-название товара -->
 							<a href="#" class="card__title">
 							Нож Mora Kansbol сталь Sandvik 12C27 рукоять резинопластик (12634)
-						</a>
-						<!-- Кнопка добавить в корзину -->
-						<button class="card__add">В корзину</button>
+							</a>
+							<!-- Кнопка добавить в корзину -->
+							<button class="card__add">В корзину</button>
+						</div>
 					</div>
 				</div>
-				<div class="col-4 card">
-					<!-- Верхняя часть -->
+				<div class="col-4">
+					<div class="card">
+						<!-- Верхняя часть -->
+						<div class="card__top">
+							<!-- Изображение-ссылка товара -->
+							<a href="#" class="card__image">
+								<img src="images/2.jpg" alt="1">
+							</a>
+						</div>
+						<!-- Нижняя часть -->
+						<div class="card__bottom">
+							<!-- Цены на товар (с учетом скидки и без)-->
+							<div class="card__prices">
+								<div class="price">4 200 &#8381;</div>
+							</div>
+							<!-- Ссылка-название товара -->
+							<a href="#" class="card__title">
+							Нож Mora Kansbol сталь Sandvik 12C27 рукоять резинопластик (12634)
+							</a>
+							<!-- Кнопка добавить в корзину -->
+							<button class="card__add">В корзину</button>
+						</div>
+					</div>
+				</div>
+				<div class="col-4">
+					<div class="card">
+						<!-- Верхняя часть -->
+						<div class="card__top">
+							<!-- Изображение-ссылка товара -->
+							<a href="#" class="card__image">
+								<img src="images/3.jpg" alt="1">
+							</a>
+						</div>
+						<!-- Нижняя часть -->
+						<div class="card__bottom">
+							<!-- Цены на товар (с учетом скидки и без)-->
+							<div class="card__prices">
+								<div class="price">4 200 &#8381;</div>
+							</div>
+							<!-- Ссылка-название товара -->
+							<a href="#" class="card__title">
+							Нож Mora Kansbol сталь Sandvik 12C27 рукоять резинопластик (12634)
+							</a>
+							<!-- Кнопка добавить в корзину -->
+							<button class="card__add">В корзину</button>
+						</div>
+					</div>
+				</div>
+				<div class="col-4">
+					<div class="card">
+						<!-- Верхняя часть -->
+						<div class="card__top">
+							<!-- Изображение-ссылка товара -->
+							<a href="#" class="card__image">
+								<img src="images/4.jpg" alt="1">
+							</a>
+						</div>
+						<!-- Нижняя часть -->
+						<div class="card__bottom">
+							<!-- Цены на товар (с учетом скидки и без)-->
+							<div class="card__prices">
+								<div class="price">4 200 &#8381;</div>
+							</div>
+							<!-- Ссылка-название товара -->
+							<a href="#" class="card__title">
+							Нож Mora Kansbol сталь Sandvik 12C27 рукоять резинопластик (12634)
+							</a>
+							<!-- Кнопка добавить в корзину -->
+							<button class="card__add">В корзину</button>
+						</div>
+					</div>
+				</div>
+				<div class="col-4">
+					<div class="card">
+						<!-- Верхняя часть -->
 						<div class="card__top">
 							<!-- Изображение-ссылка товара -->
 							<a href="#" class="card__image">
@@ -45,23 +120,142 @@
 							<!-- Ссылка-название товара -->
 							<a href="#" class="card__title">
 							Нож Mora Kansbol сталь Sandvik 12C27 рукоять резинопластик (12634)
-						</a>
-						<!-- Кнопка добавить в корзину -->
-						<button class="card__add">В корзину</button>
+							</a>
+							<!-- Кнопка добавить в корзину -->
+							<button class="card__add">В корзину</button>
+						</div>
 					</div>
 				</div>
-				<div class="col-4 card">
-					123
+				<div class="col-4">
+					<div class="card">
+						<!-- Верхняя часть -->
+						<div class="card__top">
+							<!-- Изображение-ссылка товара -->
+							<a href="#" class="card__image">
+								<img src="images/2.jpg" alt="1">
+							</a>
+						</div>
+						<!-- Нижняя часть -->
+						<div class="card__bottom">
+							<!-- Цены на товар (с учетом скидки и без)-->
+							<div class="card__prices">
+								<div class="price">4 200 &#8381;</div>
+							</div>
+							<!-- Ссылка-название товара -->
+							<a href="#" class="card__title">
+							Нож Mora Kansbol сталь Sandvik 12C27 рукоять резинопластик (12634)
+							</a>
+							<!-- Кнопка добавить в корзину -->
+							<button class="card__add">В корзину</button>
+						</div>
+					</div>
 				</div>
-				<div class="col-4 card">
-					123
+				<div class="col-4">
+					<div class="card">
+						<!-- Верхняя часть -->
+						<div class="card__top">
+							<!-- Изображение-ссылка товара -->
+							<a href="#" class="card__image">
+								<img src="images/2.jpg" alt="1">
+							</a>
+						</div>
+						<!-- Нижняя часть -->
+						<div class="card__bottom">
+							<!-- Цены на товар (с учетом скидки и без)-->
+							<div class="card__prices">
+								<div class="price">4 200 &#8381;</div>
+							</div>
+							<!-- Ссылка-название товара -->
+							<a href="#" class="card__title">
+							Нож Mora Kansbol сталь Sandvik 12C27 рукоять резинопластик (12634)
+							</a>
+							<!-- Кнопка добавить в корзину -->
+							<button class="card__add">В корзину</button>
+						</div>
+					</div>
 				</div>
+				<div class="col-4">
+					<div class="card">
+						<!-- Верхняя часть -->
+						<div class="card__top">
+							<!-- Изображение-ссылка товара -->
+							<a href="#" class="card__image">
+								<img src="images/3.jpg" alt="1">
+							</a>
+						</div>
+						<!-- Нижняя часть -->
+						<div class="card__bottom">
+							<!-- Цены на товар (с учетом скидки и без)-->
+							<div class="card__prices">
+								<div class="price">4 200 &#8381;</div>
+							</div>
+							<!-- Ссылка-название товара -->
+							<a href="#" class="card__title">
+							Нож Mora Kansbol сталь Sandvik 12C27 рукоять резинопластик (12634)
+							</a>
+							<!-- Кнопка добавить в корзину -->
+							<button class="card__add">В корзину</button>
+						</div>
+					</div>
+				</div>
+				<div class="col-4">
+					<div class="card">
+						<!-- Верхняя часть -->
+						<div class="card__top">
+							<!-- Изображение-ссылка товара -->
+							<a href="#" class="card__image">
+								<img src="images/4.jpg" alt="1">
+							</a>
+						</div>
+						<!-- Нижняя часть -->
+						<div class="card__bottom">
+							<!-- Цены на товар (с учетом скидки и без)-->
+							<div class="card__prices">
+								<div class="price">4 200 &#8381;</div>
+							</div>
+							<!-- Ссылка-название товара -->
+							<a href="#" class="card__title">
+							Нож Mora Kansbol сталь Sandvik 12C27 рукоять резинопластик (12634)
+							</a>
+							<!-- Кнопка добавить в корзину -->
+							<button class="card__add">В корзину</button>
+						</div>
+					</div>
+				</div>
+				<div class="col-4">
+					<div class="card">
+						<!-- Верхняя часть -->
+						<div class="card__top">
+							<!-- Изображение-ссылка товара -->
+							<a href="#" class="card__image">
+								<img src="images/1.jpg" alt="1">
+							</a>
+						</div>
+						<!-- Нижняя часть -->
+						<div class="card__bottom">
+							<!-- Цены на товар (с учетом скидки и без)-->
+							<div class="card__prices">
+								<div class="price">4 200 &#8381;</div>
+							</div>
+							<!-- Ссылка-название товара -->
+							<a href="#" class="card__title">
+							Нож Mora Kansbol сталь Sandvik 12C27 рукоять резинопластик (12634)
+							</a>
+							<!-- Кнопка добавить в корзину -->
+							<button class="card__add">В корзину</button>
+						</div>
+					</div>
+				</div>
+				
 			</div>
 		</div>	
 	</section>
 	<section class="container basket">
-		<div class="row">
-			
+		<div class="row card">
+			<h2>Корзина</h2>
+			<div class="col">
+				
+			</div>
 		</div>
 	</section>
 </main>
